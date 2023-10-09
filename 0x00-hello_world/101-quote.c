ef0b5d25ac9e7c32e5855bb61e4b7a41be9c1bf9
solomon <solomonnicholas71@gmail.com>
2023-10-09 06:15:24 +0000

    this is it yaeh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art useful\" - \ 
+	const char msg[] = "and that piece of art useful\" - \
 	Dora korpar, 2015-10-19\n";
 	write(STDERR_FILENO, msg, sizeof(msg) - 1);
 
