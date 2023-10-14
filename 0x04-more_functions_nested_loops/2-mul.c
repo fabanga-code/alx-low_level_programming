4da9bbaeec194bd82beb68b58b902222107488f6
solomon <solomonnicholas71@gmail.com>
2023-10-14 01:01:29 +0000

    okaymvin

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function checks for a dig (0 through).
+ * @a: var
+ * @b: var
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
