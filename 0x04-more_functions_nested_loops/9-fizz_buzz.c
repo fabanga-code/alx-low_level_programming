e5041539aa1ede390af8f50110e29d7edc5db129
solomon <solomonnicholas71@gmail.com>
2023-10-14 06:49:10 +0000

    okayoo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,4 +8,29 @@
  * Return: Always 0.
  */
 
+int main(void)
+{
+	int num;
 
+	for (num = 1; num <=100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("fizzbuzz");
+
+		else if ((num % 3) == 0)
+			printf("fizz");
+
+		else if ((num % 5) == 0)
+			printf("buzz");
+
+		else
+			printf("%d", num);
+		
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
